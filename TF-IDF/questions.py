2fcaaf6a9b252abc2a86853114938fa27d2697bf
PKSOLE <parthsolanke2002@gmail.com>
2023-06-05 17:42:27 +0530

    modified questions.py

--- TF-IDF/questions.py
+++ TF-IDF/questions.py
@@ -9,41 +9,45 @@ SENTENCE_MATCHES = 1
 
 
 def main():
+    
+    try:
+        while True:
+            # Check command-line arguments
+            if len(sys.argv) != 2:
+                sys.exit("Usage: python questions.py corpus")
 
-    # Check command-line arguments
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python questions.py corpus")
+            # Calculate IDF values across files
+            files = load_files(sys.argv[1])
+            file_words = {
+                filename: tokenize(files[filename])
+                for filename in files
+            }
+            file_idfs = compute_idfs(file_words)
 
-    # Calculate IDF values across files
-    files = load_files(sys.argv[1])
-    file_words = {
-        filename: tokenize(files[filename])
-        for filename in files
-    }
-    file_idfs = compute_idfs(file_words)
+            # Prompt user for query
+            query = set(tokenize(input("Query: ")))
 
-    # Prompt user for query
-    query = set(tokenize(input("Query: ")))
+            # Determine top file matches according to TF-IDF
+            filenames = top_files(query, file_words, file_idfs, n=FILE_MATCHES)
 
-    # Determine top file matches according to TF-IDF
-    filenames = top_files(query, file_words, file_idfs, n=FILE_MATCHES)
+            # Extract sentences from top files
+            sentences = dict()
+            for filename in filenames:
+                for passage in files[filename].split("\n"):
+                    for sentence in nltk.sent_tokenize(passage):
+                        tokens = tokenize(sentence)
+                        if tokens:
+                            sentences[sentence] = tokens
 
-    # Extract sentences from top files
-    sentences = dict()
-    for filename in filenames:
-        for passage in files[filename].split("\n"):
-            for sentence in nltk.sent_tokenize(passage):
-                tokens = tokenize(sentence)
-                if tokens:
-                    sentences[sentence] = tokens
+            # Compute IDF values across sentences
+            idfs = compute_idfs(sentences)
 
-    # Compute IDF values across sentences
-    idfs = compute_idfs(sentences)
-
-    # Determine top sentence matches
-    matches = top_sentences(query, sentences, idfs, n=SENTENCE_MATCHES)
-    for match in matches:
-        print(match)
+            # Determine top sentence matches
+            matches = top_sentences(query, sentences, idfs, n=SENTENCE_MATCHES)
+            for match in matches:
+                print(match)
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 
 def load_files(directory):
